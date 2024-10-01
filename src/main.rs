b86f3534595f26000a843aabcd8bd90d04d164d9
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-10-01 19:16:35 +0200

    implemented basics
    + user-input of file name via commandline-args
    + reading file content

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,25 @@
+use std::{env, fs, path::Path, process};
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("bf: missing arguments");
+        process::exit(1);
+    } else {
+        let inputfile = Path::new(&args[1]);
+
+        if inputfile.exists() {
+            match fs::read_to_string(inputfile) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("{e}");
+                    process::exit(1);
+                }
+            }
+        } else {
+            println!("bf: invalid arguments");
+            process::exit(1);
+        }
+    }
 }
