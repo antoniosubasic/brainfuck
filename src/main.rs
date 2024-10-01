749acc8cb368425a8be510324b8ec340098effe0
Antonio Subašić <antonio.subasic.as@gmail.com>
2024-10-01 19:43:10 +0200

    initial implementation of bf library
    + created 'Brainfuck' struct with necessary properties
    + implemented method 'new()'
    + declared method 'exec()'
    
    + implemented bf-code-execution via 'Brainfuck' struct in 'main.rs'

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{env, fs, path::Path, process};
 
+mod bf;
+use bf::Brainfuck;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,7 +14,12 @@ fn main() {
 
         if inputfile.exists() {
             match fs::read_to_string(inputfile) {
-                Ok(_) => {}
+                Ok(content) => {
+                    let brainfuck = Brainfuck::new(content, args.get(2));
+                    if let Err(e) = brainfuck.exec() {
+                        println!("{e}");
+                    }
+                }
                 Err(e) => {
                     println!("{e}");
                     process::exit(1);
